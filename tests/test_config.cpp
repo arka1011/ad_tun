a10742f73cd7db585a6b2d283681df98d34a958d
arka1011 <arka.das1994@gmail.com>
2025-12-07 15:45:22 +0530

    add state and config test cases

--- tests/test_config.cpp
+++ tests/test_config.cpp
@@ -9,9 +9,10 @@ TEST(ConfigTest, LoadValidConfig) {
 
     ASSERT_EQ(AD_TUN_OK, ad_tun_load_config("../../test_configs/good.ini", &cfg));
 
-    EXPECT_STREQ(cfg.ifname, "tun0");
-    EXPECT_STREQ(cfg.ipv4, "10.8.0.2/24");
-    EXPECT_EQ(cfg.mtu, 1500);
+    EXPECT_STREQ(cfg.ifname, "ad_tun0");
+    EXPECT_STREQ(cfg.ipv4, "10.10.1.2/24");
+    EXPECT_STREQ(cfg.ipv6, "fea0:1234:5678::9/64");
+    EXPECT_EQ(cfg.mtu, 2400);
 
     ad_tun_free_config(&cfg);
 }
@@ -21,4 +22,18 @@ TEST(ConfigTest, MissingIfnameFails) {
 
     EXPECT_EQ(AD_TUN_ERR_CONFIG,
               ad_tun_load_config("../../test_configs/missing_ifname.ini", &cfg));
+}
+
+TEST(ConfigTest, MissingIpv4Fails) {
+    ad_tun_config_t cfg;
+
+    EXPECT_EQ(AD_TUN_ERR_CONFIG,
+              ad_tun_load_config("../../test_configs/missing_ipv4.ini", &cfg));
+}
+
+TEST(ConfigTest, MissingIpv6MtuPersistAllowed) {
+    ad_tun_config_t cfg;
+
+    EXPECT_EQ(AD_TUN_OK,
+              ad_tun_load_config("../../test_configs/missing_ipv6MtuPersist.ini", &cfg));
 }
--- tests/test_state.cpp
+++ tests/test_state.cpp
@@ -19,3 +19,40 @@ TEST(StateTest, InitThenCleanup) {
     EXPECT_EQ(AD_TUN_OK, ad_tun_cleanup());
     EXPECT_EQ(AD_TUN_STATE_UNINITIALIZED, ad_tun_get_state());
 }
+
+TEST(StateTest, InitStartStopCleanup) {
+    ad_tun_config_t cfg = {
+        .ifname="ad_tun0",
+        .ipv4="10.10.0.2",
+        .ipv6="fd00:1234:5678::2/64",
+        .mtu=1500,
+        .persist=0
+    };
+
+    EXPECT_EQ(AD_TUN_OK, ad_tun_init(&cfg));
+    EXPECT_EQ(AD_TUN_STATE_INITIALIZED, ad_tun_get_state());
+
+    EXPECT_EQ(AD_TUN_OK, ad_tun_start());
+    EXPECT_EQ(AD_TUN_STATE_RUNNING, ad_tun_get_state());
+
+    EXPECT_EQ(AD_TUN_OK, ad_tun_stop());
+    EXPECT_EQ(AD_TUN_STATE_STOPPED, ad_tun_get_state());
+
+    EXPECT_EQ(AD_TUN_OK, ad_tun_cleanup());
+    EXPECT_EQ(AD_TUN_STATE_UNINITIALIZED, ad_tun_get_state());
+}
+
+TEST(StateTest, StartWithoutInitFails) {
+    EXPECT_EQ(AD_TUN_ERR_CONFIG, ad_tun_start());
+    EXPECT_EQ(AD_TUN_STATE_UNINITIALIZED, ad_tun_get_state());
+}
+
+TEST(StateTest, StopWithoutStartFails) {
+    EXPECT_EQ(AD_TUN_ERR_INVALID_STATE, ad_tun_stop());
+    EXPECT_EQ(AD_TUN_STATE_UNINITIALIZED, ad_tun_get_state());
+}
+
+TEST(StateTest, CleanupWithoutInitAccepts) {
+    EXPECT_EQ(AD_TUN_OK, ad_tun_cleanup());
+    EXPECT_EQ(AD_TUN_STATE_UNINITIALIZED, ad_tun_get_state());
+}
